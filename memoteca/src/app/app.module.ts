9a8fc6b39b65971f7abdd8ddf6210ac0f87d2f5a
brendamelodev <brendarebeka.melo@gmail.com>
2023-02-15 18:05:58 -0300

    Header, footer, formulário e estilização.

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './View/header/header.component';
+import { FooterComponent } from './View/footer/footer.component';
+import { CreateComponent } from './View/pensamentos/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
